1cb29d4b4ca9919a79781e40a2dc413b5c50d73c
suman basak <gorangobasakpkr@gmail.com>
2026-04-06 23:13:25 +0530

    File structuring

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div>
+
+      <h1 class="text-3xl text-amber-300 ">Hi there !</h1>
+      <h1 class="text-3xl font-bold">Hi there !</h1>
+      <h1 class="text-3xl font-bold">Suman Basak</h1>
+    </div>
+  )
+}
+
+export default App
