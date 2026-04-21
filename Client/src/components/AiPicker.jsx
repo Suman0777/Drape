2ec942696bd09d5444cb52d6c2f804ae0850a9c7
suman basak <gorangobasakpkr@gmail.com>
2026-04-21 21:36:03 +0530

    Ai component

--- Client/src/components/AiPicker.jsx
+++ Client/src/components/AiPicker.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
-
-const AiPicker = () => {
+import CustomButton from './CustomButton'
+const AiPicker = ({prompt, setPrompt, generatingImage, handleSubmit}) => {
   return (
-    <div>AiPicker</div>
+    <div className='aipicker-container'>
+      <textarea
+      className='aipicker-textarea'
+      placeholder='Ask AI to generate an image...'
+      rows={5}
+      value={prompt}
+      onChange={(e) => setPrompt(e.target.value)}
+      />
+      <div className='flex flex-wrap gap-3'>
+        {generatingImage ? (
+          <CustomButton
+            type="outline"
+            title= "Generating..."
+            customStyles= "text-xs"
+          />
+        ) : (
+          <>
+          <CustomButton
+            type="outline"
+            title="AI Logo"
+            handleClick={()=> handleSubmit('logo')}
+            customStyles="text-xs"
+          />
+
+          <CustomButton
+            type="filled"
+            title="AI Full"
+            handleClick={()=> handleSubmit('full')}
+            customStyles="text-xs"
+          />
+          </>
+        )}
+      </div>
+    </div>
   )
 }
 
