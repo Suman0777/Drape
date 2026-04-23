2579b62099b26856230dbc567b6cb82d4151f7e6
suman basak <gorangobasakpkr@gmail.com>
2026-04-23 19:24:45 +0530

    Server Error Changs

--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import dalleRoutes from "./routes/dalle.routes.js";
+
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-
 
 app.get("/", (req, res) => {
     res.send("Hello from DALL.E");
 });
 
-import dalleRoutes from "./routes/dalle.routes.js";
 app.use("/api/v1/dalle", dalleRoutes);
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+const PORT = process.env.PORT || 4500;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
- 
+
+process.on("uncaughtException", (err) => {
+    console.error("UNCAUGHT EXCEPTION:", err);
+});
+
+process.on("unhandledRejection", (err) => {
+    console.error("UNHANDLED REJECTION:", err);
+});
